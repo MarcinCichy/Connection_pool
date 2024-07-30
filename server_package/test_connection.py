70c80db42ed4d18fd3aabb5db977db8cb775f54f
MarcinCichy <marcin-cichy@wp.pl>
2024-07-30 14:20:31 +0200

    Checking the duration of 10,000 database connections at a time.

--- server_package/test_connection.py
+++ server_package/test_connection.py
@@ -1,20 +1,27 @@
+import time
 from connection_pool.server_package.connect import connect
-# from server_package.database_support import DatabaseSupport
+from connection_pool.server_package.database_support import DatabaseSupport
+
 
 def test_connection():
-    try:
-        # Nawiązanie połączenia z bazą danych
-        conn = connect()
-        if conn:
-            print("Połączenie z bazą danych zostało nawiązane pomyślnie.")
-            # Wykonanie prostego zapytania
-            with conn.cursor() as cur:
-                cur.execute("SELECT version();")
-                db_version = cur.fetchone()
-                print(f"Wersja bazy danych: {db_version}")
-            conn.close()
-    except Exception as e:
-        print(f"Wystąpił błąd podczas łączenia z bazą danych: {e}")
+    for _ in range(10000):
+        try:
+            # Nawiązanie połączenia z bazą danych
+            conn = connect()
+            if conn:
+                print("Połączenie z bazą danych zostało nawiązane pomyślnie.")
+                # Wykonanie prostego zapytania
+                with conn.cursor() as cur:
+                    cur.execute("SELECT version();")
+                    db_version = cur.fetchone()
+                    print(f"Wersja bazy danych: {db_version}")
+                conn.close()
+        except Exception as e:
+            print(f"Wystąpił błąd podczas łączenia z bazą danych: {e}")
+
 
 if __name__ == "__main__":
+    start_time = time.time()
     test_connection()
+    duration = time.time() - start_time
+    print(f"Duration {duration} seconds")
