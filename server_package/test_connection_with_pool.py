c4e2f5aa626ed7194c5f8806101c699bcc34f1c5
MarcinCichy <marcin-cichy@wp.pl>
2024-08-03 15:17:13 +0200

    Add method to class ConnectionPool: handle_connection_error() Change method connect() i connect.py Add test_connection_wit_pool.py

--- server_package/test_connection_with_pool.py
+++ server_package/test_connection_with_pool.py
@@ -4,19 +4,23 @@ from connection_pool.server_package.connect import connect, release_connection, 
 
 def test_connection():
     for _ in range(10000):
+
+        # Nawiązanie połączenia z bazą danych
+        conn = connect()
         try:
-            # Nawiązanie połączenia z bazą danych
-            conn = connect()
-            if conn:
-                #print("Połączenie z bazą danych zostało nawiązane pomyślnie.")
-                # Wykonanie prostego zapytania
-                with conn.cursor() as cur:
-                    cur.execute("SELECT version();")
-                    db_version = cur.fetchone()
-                    #print(f"Wersja bazy danych: {db_version}")
-                conn.close()
+            #print("Połączenie z bazą danych zostało nawiązane pomyślnie.")
+            # Wykonanie prostego zapytania
+            with conn.cursor() as cur:
+                cur.execute("SELECT version();")
+                db_version = cur.fetchone()
+                #print(f"Wersja bazy danych: {db_version}")
+            conn.close()
         except Exception as e:
-            print(f"Wystąpił błąd podczas łączenia z bazą danych: {e}")
+            # print(f"Wystąpił błąd podczas łączenia z bazą danych: {e}")
+            handle_connection_error(conn)
+            raise e
+        finally:
+            release_connection(conn)
 
 
 if __name__ == "__main__":
